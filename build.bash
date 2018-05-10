d4539f4ca81d7d0e46d95525df30ef85f5f90082
Greg Turnquist <gturnquist@pivotal.io>
2018-05-10 13:16:20 -0500

    Only deploy designated option to artifactory

--- build.bash
+++ build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" -a "${PROFILE}" = "non-existent" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" -a "${DEPLOY}" = "true" ]; then
 	mvn -Pdistribute,snapshot,docs clean deploy
 else
 	mvn clean dependency:list test -P${PROFILE} -Dsort
