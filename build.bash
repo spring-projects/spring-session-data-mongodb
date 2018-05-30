2ed746d15186b62fc203fee08d74846b807a6429
Greg Turnquist <gturnquist@pivotal.io>
2018-05-30 14:57:23 -0500

    Add a Spring Framework 5.1 profile

--- build.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" -a "${DEPLOY}" = "true" ]; then
-	mvn -Pdistribute,snapshot,docs clean deploy
-else
-	mvn clean dependency:list test -P${PROFILE} -Dsort
-fi
