6e25cfd0ac97139813f3690ce241ffa19ee54a78
Greg Turnquist <gturnquist@pivotal.io>
2018-08-03 12:03:27 -0500

    Establish 2.0.x branch
    Set up goals to verify 2.0 builds work with related stables releases and snapshots, but will also work with 2.1.

--- deploy.bash
+++ deploy.bash
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-if [ "${CIRCLE_BRANCH}" == "master" ]; then
+if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == "2.0.x" ]; then
 	mvn -Pdistribute,snapshot,docs clean deploy
 else
-	echo "We only deploy 'master' branch"
+	echo "We only deploy 'master' branch and '2.0.x' branch."
 fi
